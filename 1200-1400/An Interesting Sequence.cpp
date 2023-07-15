2451d32240ef9e9fa1888786d8272b53bdb0d6b1
Jeevesh Rai <132649861+Jeevesh0207@users.noreply.github.com>
2023-07-15 08:04:06 +0530

    Create An Interesting Sequence.cpp

---
+++ 1200-1400/An Interesting Sequence.cpp
@@ -0,0 +1,42 @@
+Question Link :- (" https://www.codechef.com/problems/INTSEQ?tab=statement ")
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FastinOut ios::sync_with_stdio(false); cin.tie(0);
+#define TimeStart auto start = chrono::high_resolution_clock::now();
+#define TimeEnd auto stop = chrono::high_resolution_clock::now();
+#define Time  long double duration = chrono::duration_cast<chrono::nanoseconds>(stop - start).count();
+
+
+void Rabbit(){
+   ll n;
+   cin>>n;
+   ll i=2,ans=0;
+   while(n%i==0){
+    ans++;
+    i*=2;
+   }
+   cout<<ans<<endl;
+}
+
+ 
+int main(){
+    FastinOut;
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    TimeStart;
+    ll T=1;
+    cin>>T;
+    while(T--){
+        Rabbit();
+    }
+    TimeEnd; 
+    Time;
+    #ifndef ONLINE_JUDGE
+        std::cerr << "Time taken : " << duration / 1e9 << "s" << std::endl;     
+    #endif
+    return 0;
+}
